ee7d40933e0d73a3afbb83971a407f13c00139a3
Seniv Volodymyr <seniv.05@gmail.com>
2025-07-16 14:43:43 +0200

    added login functionality

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
-from user.views import CreateUserView
+from rest_framework.authtoken import views
+from user.views import CreateUserView, LoginUserView
 
 
 from rest_framework import routers
@@ -8,5 +9,6 @@ from rest_framework import routers
 app_name = "user"
 
 urlpatterns = [
-    path("registration/", CreateUserView.as_view(), name="create")
+    path("registration/", CreateUserView.as_view(), name="create"),
+    path("login/", LoginUserView.as_view(), name="get_token")
 ]
--- user/views.py
+++ user/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from rest_framework import generics
 from user.serializers import UserSerializer
+from rest_framework.settings import api_settings
+from rest_framework.authtoken.views import ObtainAuthToken
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class LoginUserView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
